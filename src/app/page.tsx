1ae13ef7e078184a4b0c8c30a03d34a3e8f73ed0
Akhila Zahra <akhilazahra03@gmail.com>
2025-12-16 14:57:53 +0000

    delete temporary banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,10 @@ import ProjectsSection from '@/components/sections/projects-section';
 import CertificationsSection from '@/components/sections/certifications-section';
 import ContactSection from '@/components/sections/contact-section';
 import Footer from '@/components/footer';
-import DownloadBanner from '@/components/download-banner';
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
-      <DownloadBanner />
       <Header />
       <main className="flex-1">
         <HeroSection />
